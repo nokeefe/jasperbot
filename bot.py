cddcee0a6fd1fb01b819ad62379cb52b4f40fe1e
Nick O'Keefe <nokeefe1919@gmail.com>
2020-05-28 02:00:22 +0000

    adding more dialogue

--- bot.py
+++ bot.py
@@ -48,7 +48,19 @@ async def rip_reply(ctx):
         'Mom said its my turn on the xbox',
         'Knees weak,\nArm\'sa heavy,\nMom\'s spaghetti',
         'I don\'t feel so good Mr. Stark',
-        'BOOM HEADSHOT!'
+        'BOOM HEADSHOT!',
+        'DAMN, WHATSA MATHEMATICS, WHATSA MATHEMATICS',
+        'My name is Inigo Montoya. You killed my father. Prepare to die.',
+        'My name is Yoshikage Kira. I\'m 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don\'t smoke, but I occasionally drink. I\'m in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. I\'m trying to explain that I\'m a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn\'t lose to anyone.',
+        'Hayato.',
+        'Oh? You\'re approaching me?',
+        'Scoops. Scoops Hagendaz.',
+        'OH HE DA MANGO SENTINEL!',
+        'MAHVEL BABAY!\nOOH\nOOH\nOOH\nOOH\nHE THINKIN NEW YORK KNICKS',
+        'LEEEEEEROOOOOOOOOY JEEEEENKINSS',
+        '2 + 2 = 5 quikmafs',
+        'Samoa Joe KNOWS he doesn\'t want any of this',
+        'oof'
     ]
 
     response = random.choice(funny_messages)
